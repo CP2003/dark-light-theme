f53619733a768eedbd784b529e7047850727b01d
P A M O D <59545375+CP2003@users.noreply.github.com>
2023-08-22 18:51:18 +0530

    Create script.js

---
+++ JS/script.js
@@ -0,0 +1,23 @@
+const root = document.documentElement;
+root.setAttribute("theme", "light");
+
+const themeBtn = document.querySelector(".theme-btn");
+const dot = document.querySelector(".dot");
+const bg = document.querySelector(".bg");
+
+themeBtn.addEventListener("click" , changeTheme);
+
+function changeTheme() {
+    const currentTheme = root.getAttribute("theme");
+
+    if(currentTheme=== "dark"){
+        root.setAttribute("theme", "light");
+        dot.style.left = "0px"
+        bg.style.width = "0px"
+    }
+    if(currentTheme=== "light"){
+        root.setAttribute("theme", "dark");
+        dot.style.left = "20px"
+        bg.style.width = "40px"
+    }
+}
